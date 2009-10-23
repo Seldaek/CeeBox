d4b5677cff980af1edd84b0e95af92f0635c0356
catcubed <colin@catcubed.com>
2009-10-23 16:14:29 -0700

    Updated minimized files and comments

--- js/jquery.ceebox.js
+++ js/jquery.ceebox.js
@@ -1,6 +1,6 @@
 //ceebox
 /*
- * Ceebox 1.4.1 jQuery Plugin  - Minimized via YUI compressor
+ * Ceebox 1.4.2 jQuery Plugin  - Minimized via YUI compressor
  * Requires jQuery 1.3.2 and swfobject.jquery.js plugin to work
  * Code hosted on GitHub (http://github.com/catcubed/CeeBox) Please visit there for version history information
  * By Colin Fahrion (http://www.catcubed.com)
